3cb8bfa01d6c4a5216f91807e56f840c0c81b7f1
jun.katami <tami708j_airuy@yahoo.co.jp>
2020-09-02 08:31:04 +0900

    users_controller_refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   before_action :user_admin, only: [:index]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all.page(params[:page]).per(10)
@@ -24,16 +25,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @noodles = @user.noodles
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_update_params)
       flash[:success] = "更新成功しました"
       redirect_to @user
@@ -44,7 +42,6 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     if current_user.admin?
        @user.destroy
        flash[:success] = "アカウントを削除しました。"
@@ -60,6 +57,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
